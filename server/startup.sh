2e4349d5faab9f8334b613a94f354c16246937f8
Mykola Grybyk <mgrybyk@serena.com>
2017-05-12 12:59:45 +0300

    server: removed babel and some other packages

--- server/startup.sh
+++
@@ -1,9 +0,0 @@
-# script for supervisord, we need it because supervisor is really shitty
-echo starting via babel-node
-
-trap "{ echo Stopping babel-node; ps -ef | grep da-board | grep node | awk '{print $2}' | xargs kill -9; exit 0; }" TERM
-
-babel-node server.js &
-
-PID=$!
-wait $PID
