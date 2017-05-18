d55d0a5c818e4a787bc1e9def9ef318cfaedba11
Mykola Grybyk <mgrybyk@serena.com>
2017-05-18 13:04:02 +0300

    handle mongodb connection errors

--- server/config/modules/mongoose.js
+++ server/config/modules/mongoose.js
@@ -1,17 +1,27 @@
 'use strict'
 
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/daBoard', {
-  reconnectInterval: 15000,
-  reconnectTries: 300
-})
 mongoose.Promise = global.Promise
+
+const mongooseOpts = {
+  server: {
+    reconnectInterval: 12000,
+    reconnectTries: 300
+  }
+}
+function connectWithRetry () {
+  mongoose.connect('mongodb://localhost/daBoard', mongooseOpts).catch(err => {
+    console.error('Failed to connect to mongo on first connect, retrying in 10 sec')
+    setTimeout(connectWithRetry, 10000)
+  })
+}
+connectWithRetry()
 
 var db = mongoose.connection
 
-db.on('error', log.error.bind(console, 'Connection to mongoDb error'))
-db.once('open', function callback () {
-  log.info('Connection to mongoDb established.')
-})
+db.once('open', () => log.info('Connection to mongoDb established.'))
+db.on('error', () => log.error('Connection to mongoDb error'))
+db.on('reconnected', () => log.info('MongoDB reconnected!'))
+db.on('disconnected', () => log.warn('MongoDB disconnected!'))
 
 module.exports = db
